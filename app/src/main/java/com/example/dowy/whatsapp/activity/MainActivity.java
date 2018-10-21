6aae8346c5608fa3a8b16b2e2f1606ad0d1f8520
Doilio <doiliomatsinhe@hotmail.com>
2018-10-21 15:19:21 +0200

    Implemented FragmentStatePagerAdapter

--- app/src/main/java/com/example/dowy/whatsapp/activity/MainActivity.java
+++ app/src/main/java/com/example/dowy/whatsapp/activity/MainActivity.java
@@ -1,15 +1,86 @@
 package com.example.dowy.whatsapp.activity;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.example.dowy.whatsapp.R;
+import com.example.dowy.whatsapp.adapter.PagerAdapter;
+import com.example.dowy.whatsapp.config.ConfiguracaoFirebase;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth autenticacao = ConfiguracaoFirebase.getFirebaseAuth();
+    private TabLayout tabLayout;
+
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbarPrincipal);
+        toolbar.setTitle("WhatsApp");
+        setSupportActionBar(toolbar);
+
+        tabLayout = findViewById(R.id.tabLayoutPrincipal);
+        viewPager = findViewById(R.id.viewPagerPrincipal);
+
+        tabLayout.addTab(tabLayout.newTab().setText("Conversas"));
+        tabLayout.addTab(tabLayout.newTab().setText("Contactos"));
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+        PagerAdapter adapter = new PagerAdapter(getSupportFragmentManager(), tabLayout.getTabCount());
+
+        viewPager.setAdapter(adapter);
+
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.menuSearch:
+                break;
+            case R.id.menuConfiguracoes:
+                break;
+            case R.id.menuSair:
+                autenticacao.signOut();
+                finish();
+                break;
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
---
+++ app/src/main/java/com/example/dowy/whatsapp/adapter/PagerAdapter.java
@@ -0,0 +1,37 @@
+package com.example.dowy.whatsapp.adapter;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+import com.example.dowy.whatsapp.fragment.ContactosFragment;
+import com.example.dowy.whatsapp.fragment.ConversasFragment;
+
+public class PagerAdapter extends FragmentStatePagerAdapter {
+
+    int numOfTabs;
+
+    public PagerAdapter(FragmentManager fm, int NumOfTabs) {
+        super(fm);
+        this.numOfTabs = NumOfTabs;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0:
+                return new ConversasFragment();
+            case 1:
+                return new ContactosFragment();
+
+            default:
+                return null;
+        }
+
+    }
+
+    @Override
+    public int getCount() {
+        return numOfTabs;
+    }
+}
