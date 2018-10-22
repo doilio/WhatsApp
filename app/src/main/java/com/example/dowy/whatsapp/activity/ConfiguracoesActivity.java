a3d143d7b931271a414bcb25f73184407a44a88d
Doilio <doiliomatsinhe@hotmail.com>
2018-10-22 08:07:41 +0200

    Managing System Permissions

--- app/src/main/java/com/example/dowy/whatsapp/activity/ConfiguracoesActivity.java
+++ app/src/main/java/com/example/dowy/whatsapp/activity/ConfiguracoesActivity.java
@@ -1,18 +1,28 @@
 package com.example.dowy.whatsapp.activity;
 
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 
 import com.example.dowy.whatsapp.R;
+import com.example.dowy.whatsapp.helper.Permissao;
 
 public class ConfiguracoesActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
+    private String[] permissoesNecessarias = {Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.CAMERA};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_configuracoes);
+
+        Permissao.validarPermissoes(permissoesNecessarias, this, 1);
 
         toolbar = findViewById(R.id.toolbarPrincipal);
         toolbar.setTitle("Configuracoes");
@@ -20,5 +30,33 @@ public class ConfiguracoesActivity extends AppCompatActivity {
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        for (int permissaoResultado : grantResults) {
+            if (permissaoResultado == PackageManager.PERMISSION_DENIED) {
+                //Tratamos a permissao caso ela tenha sido negada
+                alertaValidacaoPermissao();
+            }
+        }
+    }
+
+    private void alertaValidacaoPermissao() {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Permissoes Negadas");
+        builder.setMessage("Para utilizar a app E necessario aceitar as permissoes.");
+        builder.setCancelable(false);
+        builder.setPositiveButton("confirmar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                finish();
+            }
+        });
+
+        builder.create();
+        builder.show();
     }
 }
---
+++ app/src/main/java/com/example/dowy/whatsapp/helper/Permissao.java
@@ -0,0 +1,35 @@
+package com.example.dowy.whatsapp.helper;
+
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Permissao {
+
+    public static boolean validarPermissoes(String[] permissoes, Activity activity, int requestCode) {
+
+        if (Build.VERSION.SDK_INT >= 23) {
+            List<String> listaPermissoes = new ArrayList<>();
+
+            //Percorrer as permissoes e verificar se ja tem uma liberada
+            for (String permissao : permissoes) {
+                Boolean temPermissao = ContextCompat.checkSelfPermission(activity, permissao) == PackageManager.PERMISSION_GRANTED;
+                if (!temPermissao) listaPermissoes.add(permissao);
+            }
+
+            //Caso a lista Esteja vazia nao E necessario solicitar permissao
+            if (listaPermissoes.isEmpty()) {
+                String [] novasPermissoes = new String[listaPermissoes.size()];
+                listaPermissoes.toArray(novasPermissoes);
+                //Solicita Permissao
+                ActivityCompat.requestPermissions(activity,novasPermissoes,requestCode);
+            }
+        }
+        return true;
+    }
+}
