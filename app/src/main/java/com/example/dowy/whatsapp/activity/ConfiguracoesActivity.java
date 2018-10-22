b2c7e4a2aadcc6795dc3d60784df3903695bb994
Doilio <doiliomatsinhe@hotmail.com>
2018-10-22 09:42:50 +0200

    Setting Profile Image

--- app/src/main/java/com/example/dowy/whatsapp/activity/ConfiguracoesActivity.java
+++ app/src/main/java/com/example/dowy/whatsapp/activity/ConfiguracoesActivity.java
@@ -4,8 +4,11 @@ import android.Manifest;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -16,6 +19,8 @@ import android.widget.ImageButton;
 import com.example.dowy.whatsapp.R;
 import com.example.dowy.whatsapp.helper.Permissao;
 
+import de.hdodenhof.circleimageview.CircleImageView;
+
 public class ConfiguracoesActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
@@ -24,6 +29,7 @@ public class ConfiguracoesActivity extends AppCompatActivity {
     private ImageButton galeria;
     private static final int SELECAO_CAMERA = 100;
     private static final int SELECAO_GALERIA = 200;
+    private CircleImageView fotoDePerfil;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +39,7 @@ public class ConfiguracoesActivity extends AppCompatActivity {
         Permissao.validarPermissoes(permissoesNecessarias, this, 1);
         camera = findViewById(R.id.imageButtonCamera);
         galeria = findViewById(R.id.imageButtonGallery);
+        fotoDePerfil = findViewById(R.id.roundFotoDePerfil);
 
         toolbar = findViewById(R.id.toolbarPrincipal);
         toolbar.setTitle("Configuracoes");
@@ -45,12 +52,54 @@ public class ConfiguracoesActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
                 //Verificar se foi possivel abrir o intent
-                if (i.resolveActivity(getPackageManager())!= null) {
+                if (i.resolveActivity(getPackageManager()) != null) {
                     startActivityForResult(i, SELECAO_CAMERA);
                 }
             }
         });
 
+        galeria.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent g = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                //Verificar se existe um software de galeria
+                if (g.resolveActivity(getPackageManager()) != null) {
+                    startActivityForResult(g, SELECAO_GALERIA);
+                }
+
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK) {
+
+            Bitmap imagem = null;
+
+            try {
+                switch (requestCode) {
+                    case SELECAO_CAMERA:
+                        imagem = (Bitmap) data.getExtras().get("data");
+                        break;
+                    case SELECAO_GALERIA:
+                        Uri localImagemSelecionada = data.getData();
+                        imagem = MediaStore.Images.Media.getBitmap(getContentResolver(), localImagemSelecionada);
+                        break;
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            if (imagem != null) {
+                fotoDePerfil.setImageBitmap(imagem);
+            }
+
+        }
     }
 
     @Override
