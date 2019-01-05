552aecda7fa4c9b4f9eb7329e6bc05cabd57167f
Doilio <doiliomatsinhe@hotmail.com>
2019-01-05 15:51:00 +0200

    Handling New Group Activity

--- app/src/main/java/com/example/dowy/whatsapp/activity/CadastroGrupoActivity.java
+++ app/src/main/java/com/example/dowy/whatsapp/activity/CadastroGrupoActivity.java
@@ -5,10 +5,15 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.example.dowy.whatsapp.R;
+import com.example.dowy.whatsapp.adapter.GrupoSelecionadoAdapter;
 import com.example.dowy.whatsapp.model.Usuario;
 
 import java.util.ArrayList;
@@ -17,16 +22,24 @@ import java.util.List;
 public class CadastroGrupoActivity extends AppCompatActivity {
 
     private List<Usuario> listaMembrosSelecionados = new ArrayList<>();
-
+    private TextView textTotalParticipantes;
+    private RecyclerView recyclerMembrosSelecionados;
+    private GrupoSelecionadoAdapter grupoSelecionadoAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro_grupo);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle("Novo grupo");
+        toolbar.setSubtitle("Defina o nome");
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        // Configuracoes iniciais
+        textTotalParticipantes = findViewById(R.id.textTotalParticipantes);
+        recyclerMembrosSelecionados = findViewById(R.id.recyclerMembrosGrupo);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -37,10 +50,24 @@ public class CadastroGrupoActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         // Recuperar Lista de membros passada
-        if(getIntent().getExtras() != null){
+        if (getIntent().getExtras() != null) {
             List<Usuario> membros = (List<Usuario>) getIntent().getExtras().getSerializable("membros");
             listaMembrosSelecionados.addAll(membros);
+
+            textTotalParticipantes.setText("Participantes: " + listaMembrosSelecionados.size());
         }
+
+        // Configurar recyclerview
+        grupoSelecionadoAdapter = new GrupoSelecionadoAdapter(listaMembrosSelecionados, getApplicationContext());
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(
+                getApplicationContext(),
+                LinearLayout.HORIZONTAL,
+                false
+        );
+        recyclerMembrosSelecionados.setLayoutManager(layoutManager);
+        recyclerMembrosSelecionados.setHasFixedSize(true);
+        recyclerMembrosSelecionados.setAdapter(grupoSelecionadoAdapter);
     }
 
 }
